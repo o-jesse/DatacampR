862229c5a492a6cf2266a3d0e776c8ce593d1469
o-jesse <jesse.o.au@gmail.com>
2022-04-21 20:38:23 +1000

    initial commit

---
+++ Notes.R
@@ -0,0 +1,31 @@
+sd(c(1, 5, 6, 7))
+
+values <- c(1, 5, 6, 7)
+
+values
+
+sd(values)
+
+myd_sd <- sd(values)
+
+
+sd(c(1,7))
+
+sd(
+  x = c(1,7)
+)
+
+args(sd)
+
+?sd
+help(sd)
+
+v1 <- c(1.5579,5.69945)
+v2 <- c(3.9978,6.88512)
+
+
+
+
+mean( v1 + v2, trim = 2.5)
+
+
